326a6dba48e38e5d0b1a70201764698367ddf427
heyhey1028 <shoheiogawa1028@gmail.com>
2023-07-11 08:29:04 +0900

    feat: add todo model class

---
+++ lib/models/todo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Todo {
+  const Todo({
+    required this.id,
+    required this.title,
+    this.description,
+    required this.isCompleted,
+    required this.userId,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  final String id;
+  final String title;
+  final String? description;
+  final bool isCompleted;
+  final String userId;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'].toString(),
+      title: json['title'] as String,
+      description: json['description'] as String,
+      isCompleted: json['is_completed'] as bool,
+      userId: json['user_id'] as String,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+  }
+}
